54ea2eb2d033401118b06bbf1f297b31ddc52133
Saif Addin <saif1988@gmail.com>
2021-04-28 12:29:30 +0200

    Fixed color setting players visible to players

--- modules/customization/theme.js
+++ modules/customization/theme.js
@@ -4,6 +4,31 @@ import '../../styles/customization/theme.css';
 export default class MinimalUITheme {
 
     static initSettings() {
+        new window.Ardittristan.ColorSetting("minimal-ui", "borderColor", {
+            name: game.i18n.localize("MinimalUI.BorderColorName"),
+            hint: game.i18n.localize("MinimalUI.BorderColorHint"),
+            label: game.i18n.localize("MinimalUI.ColorPicker"),
+            scope: "world",
+            restricted: true,
+            defaultColor: "#ff4900bd",
+            onChange: _ => {
+                rootStyle.setProperty('--bordercolor', game.settings.get('minimal-ui', 'borderColor'));
+            }
+        });
+
+        new window.Ardittristan.ColorSetting("minimal-ui", "shadowColor", {
+            name: game.i18n.localize("MinimalUI.ShadowColorName"),
+            hint: game.i18n.localize("MinimalUI.ShadowColorHint"),
+            label: game.i18n.localize("MinimalUI.ColorPicker"),
+            scope: "world",
+            restricted: true,
+            defaultColor: "#ff0000bd",
+            type: String,
+            onChange: _ => {
+                rootStyle.setProperty('--shadowcolor', game.settings.get('minimal-ui', 'shadowColor'));
+            }
+        });
+
         game.settings.register("minimal-ui", "shadowStrength", {
             name: game.i18n.localize("MinimalUI.ShadowStrengthName"),
             hint: game.i18n.localize("MinimalUI.ShadowStrengthHint"),
@@ -19,29 +44,6 @@ export default class MinimalUITheme {
 
     static initHooks() {
         Hooks.once('renderSceneControls', async function () {
-            new window.Ardittristan.ColorSetting("minimal-ui", "borderColor", {
-                name: game.i18n.localize("MinimalUI.BorderColorName"),
-                hint: game.i18n.localize("MinimalUI.BorderColorHint"),
-                label: game.i18n.localize("MinimalUI.ColorPicker"),
-                scope: "world",
-                defaultColor: "#ff4900bd",
-                onChange: _ => {
-                    rootStyle.setProperty('--bordercolor', game.settings.get('minimal-ui', 'borderColor'));
-                }
-            });
-
-            new window.Ardittristan.ColorSetting("minimal-ui", "shadowColor", {
-                name: game.i18n.localize("MinimalUI.ShadowColorName"),
-                hint: game.i18n.localize("MinimalUI.ShadowColorHint"),
-                label: game.i18n.localize("MinimalUI.ColorPicker"),
-                scope: "world",
-                defaultColor: "#ff0000bd",
-                type: String,
-                onChange: _ => {
-                    rootStyle.setProperty('--shadowcolor', game.settings.get('minimal-ui', 'shadowColor'));
-                }
-            });
-
             rootStyle.setProperty('--bordercolor', game.settings.get('minimal-ui', 'borderColor'));
             rootStyle.setProperty('--shadowcolor', game.settings.get('minimal-ui', 'shadowColor'));
             rootStyle.setProperty('--shadowstrength', game.settings.get('minimal-ui', 'shadowStrength') + 'px');
